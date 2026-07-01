902510ca6b7c8ebc5a4a94915f6f9e40e12e5fad
VENKAT-KIRAN-DEV <vyetigadda@gmail.com>
2026-07-01 11:30:01 +0530

    update

---
+++ intro.js
@@ -0,0 +1,47 @@
+// let day=8;
+// let dayName;
+
+// switch(day) {
+//   case 1:
+//     dayName = "Monday";
+//     break;
+//   case 2:
+//     dayName = "Tuesday";
+//     break;
+//   case 3:
+//     dayName = "Wednesday";
+//     break;
+//   case 4:
+//     dayName = "Thursday";
+//     break;
+//   case 5:
+//     dayName = "Friday";
+//     break;
+//   case 6:
+//     dayName = "Saturday";
+//     break;
+//   case 7:
+//     dayName = "Sunday";
+//     break;
+//     default:
+//     dayName = "Invalid day";
+// }
+// console.log(dayName);
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        break;
+    }
+    console.log(i);
+}
+// 
+let i = 1;
+    
+
+while (i <= 10) {
+    console.log(i);
+    i= i + 1;
+}while (i <= 10) {
+    console.log(i);
+    i++;
+}
